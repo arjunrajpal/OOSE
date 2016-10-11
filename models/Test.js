757c09d86be12c0d9de3e45deaecbe316e10f6a4
arjunrajpal <rajpal.arjun@yahoo.in>
2016-10-12 01:18:14 +0530

    Test.js module created

---
+++ models/Test.js
@@ -0,0 +1,16 @@
+var examiner = require('./examiner.js');
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var testSchema = new Schema({
+	name:String,
+	password:String,
+	examinerID:examiner,
+	questions:[{qString:String,option1:String,option2:String,option3:String,option4:String,correct:String}],
+	examinee:[{name:String,email:String,score:Number}]
+});
+
+var test = mongoose.model('test',testSchema);
+
+module.exports = test;
