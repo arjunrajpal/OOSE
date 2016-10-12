ab559d594af962d7c0448c561899813ea8ef629a
arjunrajpal <rajpal.arjun@yahoo.in>
2016-10-12 17:10:18 +0530

    viewDetails.js created

---
+++ api/viewDetails.js
@@ -0,0 +1,17 @@
+var examiner = require('../models/examiner');
+var mongoose = require('mongoose');
+
+function viewDetails(request,response)
+{
+	var examinerId = mongoose.Types.ObjectId(request.body.examinerId);
+
+	examiner.findById(examinerId,function(err,examinerDetails){
+		if(err){
+			console.log('Examiner ID:' +examinerId+':\n'+err);
+			response.json({'err':err});
+		}
+		response.json(examinerDetails);
+	});
+}
+
+module.exports = viewDetails();
