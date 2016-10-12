2fbc22ef5ab8757e90958a56f593cf94c569324c
avinav291 <avinav291@gmail.com>
2016-10-12 13:12:33 +0530

    Created Examiner & Updated Test
    @arjunrajpal look into the modifications done in test and call if u
    don’t understand

---
+++ models/Examiner.js
@@ -0,0 +1,45 @@
+
+var mongoose = require("mongoose");
+var bcrypt = require("bcrypt-nodejs");
+
+// SALT Factor for hashing passwords
+var SALT_FACTOR = 10;
+
+// Create a examiner Schema
+var examinerSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    password: {type: String, required: true},
+    phoneNo: {type: String, required: true, unique: true},
+    email: {type:String, required:true, unique:true},
+    institution:{type:String}
+});
+
+
+// Hash the password before saving
+examinerSchema.pre("save", function (done) {
+    var examiner = this;
+    if (!examiner.isModified("password")) {
+        return done();
+    }
+    bcrypt.genSalt(SALT_FACTOR, function (error, salt) {
+        if (error) {
+            return done(error);
+        }
+        bcrypt.hash(examiner.password, salt, noop, function (error, hashedPassword) {
+            if (error) {
+                return done(error);
+            }
+            examiner.password = hashedPassword;
+            done();
+        });
+    });
+});
+
+examinerSchema.methods.checkPassword = function (guess, done) {
+    bcrypt.compare(guess, this.password, function (error, isMatch) {
+        done(error, isMatch);
+    });
+};
+
+var examiner = mongoose.model("examiner", examinerSchema);
+module.exports = examiner;
--- models/Test.js
+++ models/Test.js
@@ -1,16 +1,22 @@
-var examiner = require('./examiner.js');
 
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var Schema = require("mongoose/lib/schema.js");
+var examiner = require('./Examiner.js');
 
-var testSchema = new Schema({
+//Schema
+var testSchema = mongoose.Schema({
 	name:String,
 	password:String,
-	examinerID:examiner.id,
+	examinerID:{type:Schema.Types.ObjectId, ref:'examiner'},
 	questions:[{qString:String,option1:String,option2:String,option3:String,option4:String,correct:String}],
+	password:{type:String, required:true},
+	examinerID:{type:Schema.Types.ObjectId, required:true},
+	questions:[{qString:String,option1:String,option2:String,option3:String,option4:String,correct:Number}],
 	examinee:[{name:String,email:String,score:Number}]
 });
 
+//Test Model
 var test = mongoose.model('test',testSchema);
 
+//Exports
 module.exports = test;
